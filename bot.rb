5a7d2d5574eaf8cbe578a78d07f973af05960f71
ziptofaf <ziptofaf@tuta.io>
2020-09-20 00:41:57 +0200

    initial release

---
+++ bot.rb
@@ -0,0 +1,110 @@
+# frozen_string_literal: true
+
+require 'ffi'
+require 'selenium-webdriver'
+require 'capybara'
+require 'capybara/dsl'
+require 'win32/sound'
+require 'yaml'
+require_relative 'url_builder'
+
+class Bot
+  include UrlBuilder
+  include Win32
+  include Capybara::DSL
+  attr_reader :config
+
+  def initialize_firefox_driver
+    profile = Selenium::WebDriver::Firefox::Profile.new
+    profile["permissions.default.image"] = 2
+
+    Capybara.register_driver :imageless_firefox do |app|
+      Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
+    end
+  end
+
+  def initialize
+    initialize_firefox_driver
+    Selenium::WebDriver::Firefox::Service.driver_path = "geckodriver.exe"
+    Capybara.default_driver = :imageless_firefox
+    @config = YAML.load(File.read('config.yaml'))
+  end
+
+  def scan
+    loop do
+      visit store_url(config['location'], config['card'])
+      return if config['mode'] == 'debug'
+      sleep_until_exists('.total-products-text')
+
+      text = find('.featured-container-xl')&.text || find('.featured-container-lg')&.text
+      unless text.downcase.include?('out of stock')
+        Sound.beep(300,250)
+        if config['mode'] == 'minimal'
+          sleep(600) # 10 mins to complete the purchase before browser window closes
+          return
+        end
+        add_to_cart
+        handle_checkout
+        go_to_summary
+        complete_order if config['mode'] == 'full'
+        sleep(600)
+        return
+      end
+      sleep(rand(3..5))
+    end
+  end
+
+  def add_to_cart
+    click_on(class: 'js-add-button')
+    sleep_until_exists('.js-checkout')
+    find('.js-checkout').click
+  end
+
+  def handle_checkout
+    if !page.has_css?('#billingName1') && !page.has_css?('#btnCheckoutAsGuest')
+      sleep(0.1)
+    end
+    if page.has_css?('#btnCheckoutAsGuest')
+      find('#btnCheckoutAsGuest').click
+    end
+    fill_in 'billingName1', with: config['address']['first_name']
+    fill_in 'billingName2', with: config['address']['last_name']
+    fill_in 'billingPhoneNumber', with: config['address']['phone_number']
+    fill_in 'email', with: config['address']['email']
+    fill_in 'verEmail', with: config['address']['email']
+    fill_in 'billingAddress1', with: config['address']['street']
+    fill_in 'billingAddress2', with: config['address']['apartment_number']
+    fill_in 'billingCity', with: config['address']['city']
+    fill_in 'billingPostalCode', with: config['address']['postal_code']
+    begin
+      fill_in 'billingState', with: config['address']['state'] # not all areas have this
+    rescue
+    end
+    fill_in 'ccNum', with: config['payment']['card_number']
+    fill_in 'cardSecurityCode', with: config['payment']['cvv']
+    find("#expirationDateMonth option[value='#{config['payment']['expiration_month']}']").select_option
+    find("#expirationDateYear option[value='#{config['payment']['expiration_year']}']").select_option
+  end
+
+  def go_to_summary
+    find('#dr_siteButtons input.dr_button').click
+  end
+
+  def complete_order
+    sleep_until_exists('#dr_confirmAddress')
+    p Time.now
+    #find('#dr_siteButtons input.dr_button').click
+  end
+
+  def sleep_until_exists(selector)
+    slept_for = 0
+    while !page.has_css?(selector)
+      slept_for += 0.1
+      sleep(0.1)
+    end
+  end
+end
+
+
+p Time.now
+Bot.new.scan
---
+++ readme.md
@@ -0,0 +1,12 @@
+First and foremost - go visit the config.yaml file and fill it correctly. 
+Second - you will need Firefox browser in the latest version and Geckodriver. You can obtain Geckodriver here:
+https://github.com/mozilla/geckodriver/releases
+
+Download it and unpack it to the same directory as the program. 
+
+Then let's check if bot works correctly for you - enter 'checkout' in the mode field and your actual information.
+Then put '2060S' as a card (as this one is actually available so we can go through checkout process safely)
+
+Then run the bot - within few seconds it should get you to a summary page. Ensure that product is correct and that your information is entered. 
+
+Done all that? Nice! Now and only now can you turn off the bot and the browser, change the mode to 'full' and GPU to the one you actually want to buy. 
